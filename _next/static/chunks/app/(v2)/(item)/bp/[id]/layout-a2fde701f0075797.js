3567f668ac2e3a82e9c2c9cf33ad931165cee79a
Arat5724 <arat5724@gmail.com>
2026-01-18 23:48:29 +0900

    20260118

---
+++ _next/static/chunks/app/(v2)/(item)/bp/[id]/layout-a2fde701f0075797.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2485,8909,1590,5598,6340,3894,6402,4430,9907,9607,6835],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
