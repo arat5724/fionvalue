8c98a0258fb7531bc80ec393375ecfda7d7780ef
Arat5724 <arat5724@gmail.com>
2026-05-13 00:11:57 +0900

    20260513

---
+++ _next/static/chunks/app/(v2)/(item)/box/[id]/layout-699cad81f2a6a8ee.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4430,8909,6340,5598,1590,3894,6835,2485,9907,6402,9607],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
